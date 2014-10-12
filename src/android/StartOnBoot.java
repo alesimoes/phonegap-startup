285a809dc4606a7a1397b6d27b8fb1eaf1cae98a
asxtecnologia <al.simoes@outlook.com>
2014-10-12 15:57:22 -0300

    Configuração do manifest
    Configs do manifest

--- src/android/StartOnBoot.java
+++ src/android/StartOnBoot.java
@@ -10,13 +10,24 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
 
 public class StartOnBoot extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+    	 Toast.makeText(context, "Help.Me has ben started.",
+    		        Toast.LENGTH_LONG).show();
+    		    // Vibrate the mobile phone
+    		    Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    		    vibrator.vibrate(2000);
+    		    
             Intent serviceIntent = new Intent(context, com.asxtecnologia.startup.StartUpActivity.class);
-            serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(serviceIntent);
                 }
 }
