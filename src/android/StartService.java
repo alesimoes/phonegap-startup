e4bd8ceb1b7d2d7c52ab330a6f85277dd9ebd369
asxtecnologia <al.simoes@outlook.com>
2014-10-15 21:47:03 -0300

    Serviço
    Criação do serviço

--- src/android/StartService.java
+++ src/android/StartService.java
@@ -6,30 +6,31 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-public class StartService extends Service{	
-	
-	  /** Called when the activity is first created. */
+public class StartService extends Service{  
+    
+      /** Called when the activity is first created. */
     @Override
     public void onCreate() {
         super.onCreate();
         //Log.d("TAG", "Service created.");
-        Toast.makeText(getApplicationContext(), "Service has ben started.",
-		        Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Service has ben created.",
+                Toast.LENGTH_LONG).show();
       
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //Log.d("TAG", "Service started.");
+        
+        // Nao deixa finalizar a aplicação
         int start = super.onStartCommand(intent, flags, startId);
-        
-        
+        intent = new Intent(etApplicationContext(), com.asxtecnologia.startup.StartUpActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplicationContext().startActivity(intent);
         
         //intent.addFlags(Intent.FLAG_FROM_BACKGROUND);
-        
-        Toast.makeText(getApplicationContext(), "Service has ben started.",
-		        Toast.LENGTH_LONG).show();
-        
+        Toast.makeText(getApplicationContext(), "Service command has ben started.",
+                Toast.LENGTH_LONG).show();        
        
         return start;
     }
@@ -38,7 +39,7 @@ public class StartService extends Service{
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
         Toast.makeText(getApplicationContext(), "Service has ben started.",
-		        Toast.LENGTH_LONG).show();
+                Toast.LENGTH_LONG).show();
       
         //Log.d("TAG", "Service started.");
     }
--- src/android/StartUpActivity.java
+++ src/android/StartUpActivity.java
@@ -1,18 +1,24 @@
 package com.asxtecnologia.startup;
 
 import android.os.Bundle;
-import org.apache.cordova.*;
+import android.text.method.MovementMethod;
 
+import org.apache.cordova.*;
 public class StartUpActivity extends CordovaActivity 
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        moveTaskToBack(true); 
+        //setTheme(android.R.style.Theme_NoDisplay);
         super.init();
         // Set by <content src="index.html" /> in config.xml
         super.loadUrl(Config.getStartUrl());       
+        //moveTaskToBack(true); 
        
         //super.loadUrl("file:///android_asset/www/index.html");
     }
 }
+
+
